2a2cacdb701a867b10e1d1b4df3344972d6743ec
fachririzal10 <113111212+fachririzal10@users.noreply.github.com>
2022-11-10 11:11:53 +0700

    Add files via upload

---
+++ JS11/src/latihan1_inheritance/Kedua.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan1_inheritance;
+
+/**
+ *
+ * @author Fachri
+ */
+public class Kedua extends Pertama {
+    private int b = 8;
+
+    protected void BacaSuper() {
+        System.out.print("Nilai b = " + b);
+        terprotek();
+        info();
+    } 
+}
---
+++ JS11/src/latihan3_inheritance/Hewan.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan3_inheritance;
+
+/**
+ *
+ * @author Fachri
+ */
+public class Hewan {
+    public static void testClassMethod() {
+        System.out.println("The Class Method in Hewan");
+    }
+    
+    public void testInstanceMethod() {
+        System.out.println("The Instance Method in Hewan");
+    }
+}
